97835640ba7b9372c93630884d76d27f319091ae
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2020-02-01 01:13:50 +0000

    Multiple webpacks

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./common.config');
 
-module.exports = merge(common, {
+module.exports = merge([common, {
   entry: {
     'swagger-tools': './src/index.js',
+  },
+  output: {
+    filename: '[name].min.js',
+    chunkFilename: '[name].bundle.min.js',
+    path: path.resolve(path.join(__dirname, '..', 'dist'))
   },
   // optimization: {
   //   runtimeChunk: 'single',
@@ -18,4 +24,4 @@ module.exports = merge(common, {
   //     }
   //   }
   // },
-});
+}]);
