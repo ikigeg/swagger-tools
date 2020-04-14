497eec0f0b2ed29d44701090a290350d04a2afc7
Michael Griffiths <michael@ildesigns.co.uk>
2020-04-14 08:21:59 +0100

    Prettier2 (#11)
    * Installed Prettier2
    
    * Lint fixes
    
    * 1.1.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,8 @@ module.exports = {
     node: true,
     mocha: true,
   },
-  extends: [
-    'airbnb-base', 'prettier'
-  ],
-  plugins: ['prettier', ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -18,10 +16,22 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true, trailingComma: "all" }],
-    curly: ["error", "all"],
-    "require-await": "error",
-    "no-trailing-spaces": ["error"],
-    "default-param-last": ["error"],
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, trailingComma: 'all' },
+    ],
+    curly: ['error', 'all'],
+    'require-await': 'error',
+    'no-trailing-spaces': ['error'],
+    'default-param-last': ['error'],
+    'quote-props': ["error", 'as-needed'],
   },
+  overrides: [
+    {
+      files: ['examples/**/*.js'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
 };
